cc413485b407357bf2e260a896ce93dc2920557c
Gurvir Singh <gurvirsingh791@gmail.com>
2020-08-02 15:43:22 -0400

    Add project files.

---
+++ Assignment 4/BMICalculatorForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Assignment_4
+{
+    partial class BMICalculatorForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Title_Label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Title_Label
+            // 
+            this.Title_Label.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.Title_Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Title_Label.Font = new System.Drawing.Font("Segoe UI Symbol", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Title_Label.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.Title_Label.Location = new System.Drawing.Point(26, 9);
+            this.Title_Label.Name = "Title_Label";
+            this.Title_Label.Size = new System.Drawing.Size(251, 52);
+            this.Title_Label.TabIndex = 0;
+            this.Title_Label.Text = "BMI Calculator";
+            this.Title_Label.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // BMICalculatorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(302, 433);
+            this.Controls.Add(this.Title_Label);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "BMICalculatorForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Title_Label;
+    }
+}
+
